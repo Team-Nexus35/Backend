404ebbbae0bd9315a1b246a46901f2ea7f98309e
Christiana Ibrahim <christianaibrahim011@gmail.com>
2026-02-26 00:55:05 +0100

    Debug register route

--- src/app.js
+++ src/app.js
@@ -12,6 +12,9 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.post("/api/auth/register", (req, res) => {
+  res.json({ message: "Direct route hit" });
+});
 
 app.get("/", (req, res) => {
   res.status(200).json({
